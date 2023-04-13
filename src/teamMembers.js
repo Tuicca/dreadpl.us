f3252afa2c6c9d7d421a1d653e903f8d37dbc02a
Scenci <s.cenci05@gmail.com>
2023-04-13 14:35:34 -0500

    sorted members alphabetically in the file

--- src/teamMembers.js
+++ src/teamMembers.js
@@ -1,13 +1,13 @@
 const teamMembers = [
-  { server: 'Frostmane', name: 'Icantheal' },
-  { server: 'Ner\'zhul', name: 'Yokoyama' },
+  { server: 'Tichondrius', name: 'Arcæ' },
   { server: 'Frostmane', name: 'Bigmayex' },
   { server: 'Mal\'ganis', name: 'Eremes' },
-  { server: 'Korgath', name: 'Laranthir' },
-  { server: 'Tichondrius', name: 'Arcæ' },
-  { server: 'Frostmane', name: 'Vodka' },
-  { server: 'Frostmane', name: 'Tuicca' },
+  { server: 'Frostmane', name: 'Icantheal' },
   { server: 'Frostmane', name: 'Kardoroth' },
-  { server: 'Frostmane', name: 'Leonel' }
+  { server: 'Korgath', name: 'Laranthir' },
+  { server: 'Frostmane', name: 'Leonel' },
+  { server: 'Frostmane', name: 'Tuicca' },
+  { server: 'Frostmane', name: 'Vodka' },
+  { server: 'Ner\'zhul', name: 'Yokoyama' }
 ];
 export default teamMembers
