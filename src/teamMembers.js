ebca70a28e9744e8aa52bb9491091172c747890a
steven cenci <s.cenci05@gmail.com>
2025-05-01 19:54:38 -0500

    added member

--- src/teamMembers.js
+++ src/teamMembers.js
@@ -3,6 +3,7 @@ const teamMembers = [
   { server: 'Frostmane', name: 'Cerofewol' },
   { server: 'Frostmane', name: 'Whoasked' },
   { server: 'Ner\'zhul', name: 'Yokoyama' },
-  { server: 'Caelestrasz', name: 'Mythbriyon' }
+  { server: 'Caelestrasz', name: 'Mythbriyon' },
+  { server: 'Frostmane', name: 'Kardoroth' }
 ];
 export default teamMembers
