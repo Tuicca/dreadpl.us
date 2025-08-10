808d4e2c46ab1b5aa5504e0ae6f60418af704bd5
steven cenci <s.cenci05@gmail.com>
2025-08-09 19:30:11 -0500

    quick update to memberlist

--- src/teamMembers.js
+++ src/teamMembers.js
@@ -1,7 +1,7 @@
 const teamMembers = [
   { server: 'Frostmane', name: 'Bigmayex' },
-  { server: 'Frostmane', name: 'Cerofewol' },
-  { server: 'Frostmane', name: 'Whoasked' },
+  { server: 'Frostmane', name: 'Brewmaster' },
+  { server: 'Frostmane', name: 'Vodka' },
   { server: 'Ner\'zhul', name: 'Yokoyama' },
   { server: 'Ner\'zhul', name: 'Eremes' },
   { server: 'Caelestrasz', name: 'Mythbriyon' },
