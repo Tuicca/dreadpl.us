4f11c61e8df32c16ae653e226efea8f80ee5fb72
steven cenci <s.cenci05@gmail.com>
2025-08-15 20:12:42 -0500

    added players

--- src/teamMembers.js
+++ src/teamMembers.js
@@ -4,5 +4,11 @@ const teamMembers = [
   { server: 'Ner\'zhul', name: 'Yokoyama' },
   { server: 'Ner\'zhul', name: 'Eremes' },
   { server: 'Frostmane', name: 'Kardoroth' },
+  { server: 'Ner\'zhul', name: 'Arcae' },
+  { server: 'Zul\'jin', name: 'Jazzpie' },
+  { server: 'Frostmane', name: 'PoopaVlad' },
+  { server: 'Frostmane', name: 'Pugchimp' },
+  { server: 'Ner\'zhul', name: 'Scribbl' }
+
 ];
 export default teamMembers
